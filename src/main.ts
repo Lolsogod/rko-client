d98e7052003e85194ff8b84164ce387bd84fe163
COCKTAIL <aristovstas2004@gmail.com>
2023-07-09 06:06:02 +0400

    so-so...keycloak is folling down

--- src/main.ts
+++ src/main.ts
@@ -6,14 +6,35 @@ import '@factoringplus/pl-components-pack-v3/dist/style.css';
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
+import VueKeyCloak from '@dsb-norge/vue-keycloak-js'
 import App from './App.vue'
 import router from './router'
+import type {VueKeycloakInstance} from "@dsb-norge/vue-keycloak-js/dist/types";
 
-const app = createApp(App)
+const keycloakOptions = {
+    config: {
+        url: 'http://keycloak.yamakassi.ru/auth',
+        realm: 'claimapi',
+        clientId: 'cocktail',
 
+    },
+    initOptions: {
+        // redirectUri:"http://localhost:5173/",
+        onLoad: 'login-required',
+        clientId: 'cocktail',
+        // clientSecret: 'yP0hhCKnmtYrczj6eIuIKtvvO51pwbNaG', // Replace with your actual client secret
+        // username: 'usersupervisor', // Replace with your actual username
+        // password: '1234', // Replace with your actual password
+    },
+};
+const app = createApp(App).use(VueKeyCloak, keycloakOptions)
 app.use(createPinia())
 app.use(router)
 app.use(components)
+declare module '@vue/runtime-core' {
+    interface ComponentCustomProperties  {
+        $keycloak: VueKeycloakInstance
+    }
+}
 
 app.mount('#app')
