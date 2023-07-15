3445382fef7cb5b1490c299ff86d8e560e34b064
DIO <46161640+Lolsogod@users.noreply.github.com>
2023-07-15 10:45:31 +0400

    finaly fix wierd behavior of lib directory

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,6 @@
-import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import tsconfigPaths from 'vite-tsconfig-paths';
-import path from 'node:path';
 
 export default defineConfig({
   plugins: [vue(),tsconfigPaths({loose: true})]
