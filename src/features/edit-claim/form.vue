4e465e138ee4dc369d2e52f331f37600fd02c1e5
COCKTAIL <aristovstas2004@gmail.com>
2023-07-28 19:42:20 +0400

    fix edit ids

--- src/features/edit-claim/form.vue
+++ src/features/edit-claim/form.vue
@@ -8,7 +8,9 @@ import { useClaimConfig } from 'entities/claim';
 defineProps(['claimId', 'clientId'])
 const {claim} = storeToRefs(useEditClaimStore())
 const {refernces} = useReferenceStore()
-const rules = {}
+const rules = {
+
+}
 const category = computed(()=>{
     return refernces!.categories.find((item: ReferenceData) => item.code == claim.value!.category)!.text
 })
@@ -16,7 +18,7 @@ const formRef = ref(null);
 </script>
 
 <template>
-    <PlForm  v-if="claim" :key="claim.id" ref="formRef" :model="claim"  class="d-grid gap-6 w-100" :rules="rules">
+    <PlForm  v-if="claim && claim?.client" :key="claim.id" ref="formRef" :model="claim"  class="d-grid gap-6 w-100" :rules="rules">
         <div class="d-grid gap-4" style="grid-template-columns: 1fr 1fr .25fr">
             <DropDown label="Канал сообщений" 
             :options="refernces?.channels!"
