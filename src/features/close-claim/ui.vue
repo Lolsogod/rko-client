cec46340e70d7c3ab66a3f0ebf8a490e458dea07
COCKTAIL <aristovstas2004@gmail.com>
2023-07-29 04:19:53 +0400

    add validation to close

--- src/features/close-claim/ui.vue
+++ src/features/close-claim/ui.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { computed, ref } from 'vue';
+import {computed, ref, watch} from 'vue';
 import { DropDown } from "shared/ui/drop-down"
 import { useReferenceStore, type ReferenceData } from 'entities/reference';
 import { storeToRefs } from 'pinia';
@@ -16,43 +16,74 @@ const {closeModal} = useModalStore();
 const claimId = ref({
   id:useRoute().params.claimId
 })
-const rules = {
-
-}
+const formatter = (value) => {
+  return value.replace(/D/g, "");
+};
+const checkMaxSum = (rule, value, callback) => {
+  if (value.length < 5) {
+    callback(new Error("Оставьте коментарий подлинне"));
+  }
+  callback();
+};
+const rules = ref({
+  comment : [{
+    required:true,
+    validator:checkMaxSum
+  }],
+  status : [{
+    required:true,
+    message:"Обязалово"
+  }],
+  status_reason : [{
+    required:false,
+    message:"Обязалово"
+  }]
+})
 const ncForm = ref<CloseReq>({
   status:'',
   comment: '',
-  status_reason:''
+  status_reason:'',
 });
+const disabled = ref(true);
 const curReasons = computed(() => getStatusesReasonsByStatus(ncForm.value.status))
-
-const formRef = ref(null);
-const close = () => {
-  const id  = +claimId.value.id;
-  console.log(id)
-  if(!isNaN(id)){
-    closePost(ncForm.value, id);
-    ncForm.value.status = '';
-    ncForm.value.status_reason = '';
-    ncForm.value.comment = '';
-    closeModal();
-  }
-}
-const disabled = computed(() => {
-  const closeReq = ncForm.value;
-  console.log(curReasons.value.length < 1)
-  if(!curReasons.value || curReasons.value.length! < 1){
-    return (closeReq.status == '' || closeReq.comment == '')
-  }
-  for(const [_,val] of Object.entries(closeReq)){
-    if(val === '' || !val) return true;
-  }
-  return false;
+watch(curReasons, (newV,oldV) => {
+    if(newV && newV.length>0){
+      rules.value.status_reason[0].required = true;
+    }else{
+      rules.value.status_reason[0].required = false;
+    }
 })
+watch(ncForm, () => {
+  formRef?.value?.validate((v) => {
+    disabled.value = !v;
+  })
+}, {deep:true})
+const formRef = ref<{validate:(v:any)=>void}|null>(null);
+const close = () => {
+  formRef?.value?.validate((v) => {
+    disabled.value = !v;
+    if(v){
+      const id  = +claimId.value.id;
+      if(!isNaN(id)){
+        try{
+          closePost(ncForm.value, id);
+          ncForm.value.status = '';
+          ncForm.value.status_reason = '';
+          ncForm.value.comment = '';
+          closeModal();
+        }catch (e){
+          ncForm.value.comment = 'Что-то пошло не так';
+        }
+
+      }
+    }
+  })
+
+}
 </script>
 
 <template>
-  <PlForm model="ncForm" :style="{minHeight:'500px', minWidth:'755px'}" ref="formRef"   class="d-grid gap-6 w-100" :rules="rules">
+  <PlForm :model="ncForm" :style="{minHeight:'500px', minWidth:'755px'}" ref="formRef"   class="d-grid gap-6 w-100" :rules="rules">
     <div class="d-grid gap-4" style="grid-template-columns: 1fr 1fr .25fr">
       <DropDown label="Статус"
                 v-model = "ncForm.status"
@@ -66,6 +97,7 @@ const disabled = computed(() => {
     <div class="d-flex gap-4">
       <PlInputPlus
           textarea
+          :formatter = "formatter"
           v-model="ncForm.comment"
           prop="comment"
           label="Коментарий"
